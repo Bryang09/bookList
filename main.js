8deea1c7347a270adf6f488c6d0c75a4662955c3
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-12-24 19:37:31 -0600

    Alert Message Included

--- main.js
+++ main.js
@@ -42,6 +42,22 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+
+    container.insertBefore(div, form);
+
+    // VANISH IN 3 SECONDS
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
+
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
@@ -64,18 +80,29 @@ document.querySelector("#book-form").addEventListener("submit", e => {
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  // INSTANTIATE BOOK
-  const book = new Book(title, author, isbn);
+  // VALIDATE
+  if ((title === "" || author === "", isbn === "")) {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    // INSTANTIATE BOOK
+    const book = new Book(title, author, isbn);
 
-  // ADD BOOK TO UI
-  UI.addBookToList(book);
+    // ADD BOOK TO UI
+    UI.addBookToList(book);
 
-  // CLEAR FIELDS
-  UI.clearFields();
+    // SHOW SUCCESS MESSAGE
+    UI.showAlert("Book Added", "success");
+
+    // CLEAR FIELDS
+    UI.clearFields();
+  }
 });
 
 // EVENT: REMOVE A BOOK
 
 document.querySelector("#book-list").addEventListener("click", e => {
   UI.deleteBook(e.target);
+
+  // SHOW DELETE MESSAGE
+  UI.showAlert("Book Removed", "primary");
 });
